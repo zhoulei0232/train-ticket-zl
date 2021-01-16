ba62b04917ace5bbc945911cdb5a5c4bd717a1a3
zhoulei0232 <5971794@qq.com>
2021-01-16 11:42:37 +0800

    同步代码

---
+++ src/index/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom  from 'react-dom';
+import {Provider}  from 'react-redux';
+import 'normalize.css/normalize.css';
+
+import store from './store';
+import './index.css';
+import App from './App.jsx';
+
+ReactDom.render(
+    <Provider  store={store}><App/></Provider>,document.getElementById('root')
+    );
